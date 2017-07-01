9c06675f85835ce1fdb2a7423337a233b6a841ba
Patrick Oryono <patricken08@gmail.com>
2017-07-01 16:03:03 +0300

    Removed download button on video tag

--- resources/views/challenge.blade.php
+++ resources/views/challenge.blade.php
@@ -15,7 +15,7 @@
                 <div>
                     <h4>{{"Day ". $challenge->day . " ". $challenge->name }}</h4>
                 </div>
-                <video controls style="width: 100%;" autoplay preload = "auto">
+                <video controls style="width: 100%;" autoplay preload = "auto" controlsList="nodownload">
                     <source src="{{ url($challenge->video) }}" type="video/mp4">
                     <source src="{{ url($challenge->video) }}" type="video/ogg">
                     Your browser does not support the video tag.
