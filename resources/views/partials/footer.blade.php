75e2e633ce11d3ef00134088222e7e5de75ac300
Patrick Oryono <patricken08@gmail.com>
2017-07-03 18:16:03 +0300

    corrected phone numbers

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <div id="footer">
     <div class="container">
-        <p>© Copyright 2017. Kingdom Challenge All Rights Reserved | <a href="tel:+256776717444">+256776717444</a> | <a
-                    href="tel:+256705717474">+256705717474</a> | Plot 7 Nakawa House, Ground floor, Port Bell Rd | <a
+        <p>© Copyright 2017. Kingdom Challenge All Rights Reserved | <a href="tel:+256787908207">+256787908207</a> | <a
+                    href="tel:+256751747532">+256751747532</a> | Plot 7 Nakawa House, Ground floor, Port Bell Rd | <a
                     href="{{ url('contact') }}">Contact Us</a>
         
         </p>
